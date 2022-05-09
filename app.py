7a044a7221217dd79175259857cef1892a8e2a39
sahanasjay <70827533+sahanasjay@users.noreply.github.com>
2022-05-09 01:20:00 -0400

    adding....many changes

--- app.py
+++ app.py
@@ -9,20 +9,26 @@ from bs4 import BeautifulSoup
 from slack.errors import SlackApiError
 import csv
 from pprint import pprint
+import dateparser
 
 
 db = Database("food_inspections.db")
-inspections = db["inspections"]
-cursor = db.execute("""update inspections set inspection_date = substr(inspection_date,7,4)||'-'||substr(inspection_date, 4,2)||'-'||substr(inspection_date, 1,2)""")
-print(cursor.rowcount)
-#print(db.schema)
 
+def get_max_date():
+    for row in db.query("""select max(date) from inspections"""):
+        max_date = list(row.values())
+    return max_date
+
+'''
 def get_max_row_id():
-    for row in db.query("""select max(rowid) from inspections order by inspection_date desc"""):
+    for row in db.query("""select max(rowid) from inspections"""):
         max_id = list(row.values())
     return max_id
 
+print(get_max_row_id())
+
 #print(get_max_row_id())
+'''
 
 def get_ids_in_db():
     uid_list = []
@@ -32,7 +38,6 @@ def get_ids_in_db():
 
     return uid_list
 
-max_id = get_max_row_id()
 
 def get_inspections():
     infile = open("food_inspections.csv", newline='')
@@ -40,7 +45,8 @@ def get_inspections():
     uid_list = get_ids_in_db()
     db_list = []
     main_list = []
-    max_id = get_max_row_id()
+    max_date = get_max_date()
+    print(max_date)
     for item in reader:
         item =  {k.lower(): v for k, v in item.items()}
         if 'COLLEGE PARK' in item['city']:
@@ -50,7 +56,9 @@ def get_inspections():
             else:
                 item['coordinates'] = []
 
+
             item['uid'] = item['establishment_id']+'-'+item['inspection_date']
+            item['date'] = datetime.datetime.strftime(dateparser.parse(item['inspection_date']), '%Y-%m-%d')
             #db_list.append(item)
 
             if item['uid'] in uid_list:
@@ -62,20 +70,28 @@ def get_inspections():
 
     #values = ['Critical Violations observed','Non-Compliant - Violations Observed']
 
-    for row in db.query("select * from inspections where inspection_results in ('Critical Violations observed','Non-Compliant - Violations Observed') and rowid > ? order by inspection_date desc", max_id):
+    for row in db.query("""select * from inspections where inspection_results in ('Critical Violations observed','Non-Compliant - Violations Observed') and date > ? order by date""", max_date):
+            #row['violation']
             main_list.append(row)
 
     return main_list
 
 #print(len(get_inspections()))
-pprint(get_inspections())
-
+#nmprint(get_inspections())
 
 
 def send_slack_msg():
     for_message = get_inspections()
+    max_date = get_max_date()
+    restaurant_names = []
     for item in for_message:
+        violations = []
+        for k,v in item.items():
+            if v == 'Out of Compliance':
+                violations.append(k)
+        item['violations'] = violations
         estab_id = item['establishment_id']
+        restaurant_names.append(item['name'].lower())
         query = f"select * from inspections where establishment_id={estab_id}"
         previous = [item for item in db.query(query)]
         values = ['Critical Violations observed','Non-Compliant - Violations Observed']
@@ -84,11 +100,13 @@ def send_slack_msg():
         number_prev_violations = len(prev_violations)
         item['previous_number'] = number_previous_value
         item['prev_violations'] = number_prev_violations
-
+        print(item)
     main_msg = len(for_message)
-    restaurant_names = [item['name'] for item in db.query("select name from inspections where inspection_results in ('Critical Violations observed','Non-Compliant - Violations Observed') and rowid > ?", max_id)]
-    message = f":rotating_light: *Food Inspection Summary* :rotating_light:\n Hello <!channel>, Bot here with your weekly summary.\n This week, there were *{main_msg}* inspections in College Park resulting in a violation.\n Affected businesses include: {restaurant_names} \n See my :thread: for details."
+    restaurant_names = '\n- '.join(restaurant_names)
+    #print(restaurant_names)
+    message = f":rotating_light: *FOOD INSPECTION SUMMARY* :rotating_light:\n\n:wave:Hello channel, Bot here with your weekly summary.\n\n This week, there were *{main_msg}* inspections in College Park resulting in a violation.\n\n Affected businesses include:\n- {restaurant_names}\n\nSee my :thread: for details."
     client = WebClient()
+    #<!channel>
     slack_token = os.environ["SLACK_API_TOKEN"]
     client = WebClient(token=slack_token)
     ts_id = ""
@@ -104,6 +122,8 @@ def send_slack_msg():
     #You will get a SlackApiError if "ok" is False
             assert e.response["error"]
         for item in for_message:
+            violations = item['violations']
+            violations = '\n- '.join(violations)
             try:
                 #estab_id = item['establishment_id']
                 #number_previous = [item for item in db.query("select count(*) from inspections where establishment_id=?", estab_id)]
@@ -113,7 +133,7 @@ def send_slack_msg():
                 response = client.chat_postMessage(
                 channel="slack-bots",
                 thread_ts=ts_id,
-                blocks = [{"type": "section", "text": {"type": "mrkdwn", "text": f"A {item['inspection_type']} inspection took place at {item['name']} on {item['inspection_date']}, and the result was {item['inspection_results']}\nThis establishment has been through {item['previous_number']} inspections, {item['prev_violations']} of which resulted in a violation.\nFor more: <https://data.princegeorgescountymd.gov/Health/Food-Inspection/umjn-t2iz|Here's the link to the data>.\nWant to file an MPIA? <https://www.princegeorgescountymd.gov/DocumentCenter/View/4629/MPIA-Request-Form-PDF| Here's a form> for that."}}]
+                blocks = [{"type": "section", "text": {"type": "mrkdwn", "text": f"A {item['inspection_type']} inspection took place at {item['name']} on {item['date']}, and the result was *{item['inspection_results']}*.\n\nMeasures out of compliance were: \n- {violations}\n\nThis establishment has been through *{item['previous_number']}* inspections, *{item['prev_violations']}* of which resulted in a violation.\n\nFor more: <https://data.princegeorgescountymd.gov/Health/Food-Inspection/umjn-t2iz|Here's the link to the data>.\n\nWant to file an MPIA? <https://www.princegeorgescountymd.gov/DocumentCenter/View/4629/MPIA-Request-Form-PDF| Here's a form> for that."}}]
                 #text=f"🚨 Food Inspection result: {item['inspection_type']} 🚨 Inspectiontook place at {item['name']} on {item['inspection_date']}, and the result was {item['inspection_results']}."
                 )
             except SlackApiError as e:
@@ -123,7 +143,7 @@ def send_slack_msg():
         try:
             response = client.chat_postMessage(
             channel="slack-bots",
-            blocks = [{"type": "section", "text": {"type": "mrkdwn", "text": f":wave: Hello there! Bot here with your weekly summary.\n*Nothing to report this week*: There were *{main_msg}* inspections that resulted in a violation in College Park. Check back in next week!"}}]
+            blocks = [{"type": "section", "text": {"type": "mrkdwn", "text": f":wave: Hello there! Bot here with your weekly summary.\n\n*Nothing to report this week*: There were *{main_msg}* inspections that resulted in a violation in College Park. Check back in next week!"}}]
             #text=f"🚨 Food Inspection result: {item['inspection_type']} 🚨 Inspectiontook place at {item['name']} on {item['inspection_date']}, and the result was {item['inspection_results']}."
             )
         except SlackApiError as e:
@@ -148,5 +168,5 @@ send_slack_msg()
         #except SlackApiError as e:
     #You will get a SlackApiError if "ok" is False
             #assert e.response["error"]
-'''
+
 #make_thread()
